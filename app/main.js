0bc98e2c1034ea051060caa98058e13743942275
Janine Hempy <jhempy@gmail.com>
2017-09-28 14:09:33 -0400

    Finished the game

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ var restartButton = document.getElementById('restartButton');
 var spaces = document.getElementsByClassName('space');
 var symbols = ['O', 'X'];
 var turn = 0;
+var notification = document.getElementById('notification');
+var winnerMessage = document.getElementById('winnerMessage');
 
 // Waits for page to load before doing interesting things
 document.onreadystatechange = function() {
@@ -18,7 +20,8 @@ function startGame() {
   turn = 0;
 
   // Remove winner notification and make sure div is hidden
-
+  winnerMessage.innerHTML = '';
+  notification.style.display = 'none';
 
   // Clear board and add click events on squares
   for (var i = 0; i < spaces.length; i++) {
@@ -31,12 +34,28 @@ function startGame() {
 function takeSpace() {
 
   turn++;
-  this.innerHTML = symbols[turn % 2];
+  var currentPlayer = symbols[turn % 2];
+  this.innerHTML = currentPlayer;
   this.removeEventListener("click", takeSpace);  
 
   for (var i = 0; i < wins.length; i++) {
     if (checkForWin(wins[i])) {
-      alert("Whoo hoo! Someone won!");
+
+      // No more clicking!
+      for (var j = 0; j < spaces.length; j++) {
+        spaces[j].removeEventListener("click", takeSpace);
+      }
+
+      // Notify the players
+      notification.style.display = 'block';
+      winnerMessage.innerHTML = "Yay! " + currentPlayer + " won!";
+
+    }
+    else {
+      if (turn == 9) {
+        notification.style.display = "block";
+        winnerMessage.innerHTML += "Bummer! It's a draw!";
+      }
     }
   }
 
@@ -44,9 +63,9 @@ function takeSpace() {
 
 function checkForWin(winArray) {
     
-  return spaces[winArray[0]] !== '' && 
-    spaces[winArray[0]] === spaces[winArray[1]] && 
-    spaces[winArray[0]] === spaces[winArray[2]];
+  return spaces[winArray[0]].innerHTML !== '' && 
+    spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML && 
+    spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
   
 }
 
