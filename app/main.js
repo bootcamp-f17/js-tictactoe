0c4d950f48f81434bcbb68b842eb7da05819e353
Janine Hempy <jhempy@gmail.com>
2017-09-28 11:32:14 -0400

    User can click on spaces and record moves

---
+++ app/main.js
@@ -0,0 +1,39 @@
+// Globals
+var restartButton = document.getElementById('restartButton');
+var spaces = document.getElementsByClassName('space');
+var symbols = ['O', 'X'];
+var turn = 0;
+
+// Waits for page to load before doing interesting things
+document.onreadystatechange = function() {
+  if (document.readyState == "interactive") {
+    restartButton.onclick = startGame;
+    startGame();
+  }
+};
+
+function startGame() {
+
+  // Reset turn counter
+  turn = 0;
+
+  // Remove winner notification and make sure div is hidden
+
+
+  // Clear board and add click events on squares
+  for (i = 0; i < spaces.length; i++) {
+    spaces[i].innerHTML = '';
+    spaces[i].addEventListener("click", takeSpace);
+  }  
+
+}
+
+function takeSpace() {
+
+  turn++;
+  this.innerHTML = symbols[turn % 2];
+  this.removeEventListener("click", takeSpace);  
+
+}
+
+
